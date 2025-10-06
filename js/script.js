a6f90fde54e4cf1108b4cdce06af4f78615d057a
Buinov Vadym <karabincom@gmail.com>
2025-10-06 13:43:11 +0300

    Save progress

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
+const burgerBtn = document.getElementById("burgerBtn");
+const closeBurgerBtn = document.getElementById("closeBurgerBtn");
+const burgerMenu = document.getElementById("burgerMenu");
+
 const modalBtn = document.getElementById("modalBtn");
 const backDrop = document.getElementById("backDrop");
 const closeModalBtn = document.getElementById("closeModalBtn");
+
 const bodyEl = document.body;
 
-modalBtn.addEventListener("click", toggleModal);
-closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  backDrop.classList.toggle("is-open");
-
-  if (backDrop.classList.contains("is-open")) {
+function updateScrollLock() {
+  if (
+    burgerMenu.classList.contains("is-open") ||
+    backDrop.classList.contains("is-open")
+  ) {
     bodyEl.style.overflow = "hidden";
   } else {
     bodyEl.style.overflow = "visible";
   }
 }
+
+function toggleBurger() {
+  burgerMenu.classList.toggle("is-open");
+  updateScrollLock();
+}
+
+function toggleModal() {
+  backDrop.classList.toggle("is-open");
+  updateScrollLock();
+}
+
+burgerBtn.addEventListener("click", toggleBurger);
+closeBurgerBtn.addEventListener("click", toggleBurger);
+
+modalBtn.addEventListener("click", toggleModal);
+closeModalBtn.addEventListener("click", toggleModal);
+
+backDrop.addEventListener("click", (e) => {
+  if (e.target === backDrop) {
+    toggleModal();
+  }
+});
